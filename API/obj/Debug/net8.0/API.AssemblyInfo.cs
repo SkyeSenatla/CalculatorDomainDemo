7cde75e3d032c62e696343db0bdd4998c072dc2d
Skye <2018389656@ufs4life.ac.za>
2026-02-15 22:34:41 +0200

    Day 1 Demo guide

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc88e696ad7699e0f98f499c512d0662bc570155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4f8061b3b7bd1186715e45bed0722532d83d39")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
